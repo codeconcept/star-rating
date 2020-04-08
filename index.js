4e924a63c88a3447ddbaefe8c358a1e2698ac9b9
codeconcept <contact@codeconcept.fr>
2020-04-08 17:05:25 +0200

    Add / remove css on a star

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ init();
 function init() {
   allStars.forEach((star) => {
     star.addEventListener("click", getRating);
+    star.addEventListener("mouseover", addCSS);
+    star.addEventListener("mouseleave", removeCSS);
   });
 }
 
@@ -13,4 +15,12 @@ function getRating(e) {
   console.log(e, e.target);
   console.dir(e.target);
   console.log(e.target.dataset);
+}
+
+function addCSS(e, css = "checked") {
+  e.target.classList.add(css);
+}
+
+function removeCSS(e, css = "checked") {
+  e.target.classList.remove(css);
 }

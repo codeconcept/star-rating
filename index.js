4bd7b77a6306c615cd4466aa2c6f86fb6b68953c
codeconcept <contact@codeconcept.fr>
2020-04-08 17:35:36 +0200

    Add / remove css to all previous siblings

--- index.js
+++ index.js
@@ -12,15 +12,34 @@ function init() {
 }
 
 function getRating(e) {
-  console.log(e, e.target);
+  console.log(e, e.target, e.target.nodeName, e.target.nodeType);
   console.dir(e.target);
   console.log(e.target.dataset);
 }
 
 function addCSS(e, css = "checked") {
-  e.target.classList.add(css);
+  const overedStar = e.target;
+  overedStar.classList.add(css);
+  const previousSiblings = getPreviousSiblings(overedStar);
+  console.log("previousSiblings", previousSiblings);
+  previousSiblings.forEach((elem) => elem.classList.add(css));
 }
 
 function removeCSS(e, css = "checked") {
-  e.target.classList.remove(css);
+  const overedStar = e.target;
+  overedStar.classList.remove(css);
+  const previousSiblings = getPreviousSiblings(overedStar);
+  previousSiblings.forEach((elem) => elem.classList.remove(css));
+}
+
+function getPreviousSiblings(elem) {
+  console.log("elem.previousSibling", elem.previousSibling);
+  let sibs = [];
+  const spanNodeType = 1;
+  while ((elem = elem.previousSibling)) {
+    if (elem.nodeType === spanNodeType) {
+      sibs = [elem, ...sibs];
+    }
+  }
+  return sibs;
 }
